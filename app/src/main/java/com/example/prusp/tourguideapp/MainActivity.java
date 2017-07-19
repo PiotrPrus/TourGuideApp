08affee8e7e649583a5048843c767df7c08e5348
Piotr Prus <prus.piotr@gmail.com>
2017-07-19 22:58:16 +0200

    Initial commit, viewPager implemented and working correctly

---
+++ app/src/main/java/com/example/prusp/tourguideapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.prusp.tourguideapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initialViewPagerWithTabs();
+    }
+
+    private void initialViewPagerWithTabs() {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/prusp/tourguideapp/SampleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.prusp.tourguideapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by prusp on 19.07.2017.
+ */
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter{
+
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[] {"Restaurants", "Nature", "Cultural", "Architecture"};
+    private Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
